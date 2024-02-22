8ef4ea056a0c9884582075d8cf521461cd4d7298
bhavik doshi <82199211+bhavik25-cpu@users.noreply.github.com>
2024-02-22 22:29:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config(); 
 const express = require('express');
 const bodyParser = require('body-parser');
 const reportRoutes = require('./routes/reportRoutes');
