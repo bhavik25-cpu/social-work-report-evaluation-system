87961f658f51449cc6ec2388f1fd51b225df8c7c
bhavik doshi <82199211+bhavik25-cpu@users.noreply.github.com>
2024-02-22 22:32:28 +0530

    Update reportRoutes.js

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -3,19 +3,14 @@ const router = express.Router();
 const { validateCreateReport, validateUpdateReport, handleValidationErrors } = require('../middleware/validationMiddleware');
 const reportController = require('../controller/reportController');
 
-// Create a new report
 router.post('/', validateCreateReport, handleValidationErrors, reportController.createReport);
 
-// Get all reports
 router.get('/', reportController.getAllReports);
 
-// Get report by ID
 router.get('/:id', reportController.getReportById);
 
-// Update report by ID
 router.put('/:id', validateUpdateReport, handleValidationErrors, reportController.updateReport);
 
-// Delete report by ID
 router.delete('/:id', reportController.deleteReport);
 
 router.post('/updatePoints', reportController.updatePoints);
