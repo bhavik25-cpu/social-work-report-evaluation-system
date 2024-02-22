4d06821f1d102c6157a3f358bc2b29b2f4db150e
bhavik doshi <82199211+bhavik25-cpu@users.noreply.github.com>
2024-02-22 22:30:38 +0530

    Update dbConfig.js

--- DB/dbConfig.js
+++ DB/dbConfig.js
@@ -1,6 +1,5 @@
 const mysql = require('mysql');
 
-// Database connection configuration
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -8,7 +7,6 @@ const connection = mysql.createConnection({
   database: process.env.DB_NAME
 });
 
-// Connect to the database
 connection.connect((err) => {
   if (err) {
     console.error('Error connecting to database: ' + err.stack);
